599e297e75fcefe5f48bbb353e8a13493083cf1c
jmickle66666666 <jaymickle@gmail.com>
2016-06-16 17:51:21 +0100

    make it work again (i broke it)

--- twitter.js
+++ twitter.js
@@ -7,17 +7,15 @@ function randomColor() {
 	return Math.floor(Math.random() * 16777216).toString(16);
 }
 
-document.onLoad = function () {
+window.onload = function () {
+	var fn = getName();
+	var un = getUsername(fn);
 
-var fn = getName();
-var un = getUsername(fullname);
-
-document.getElementById('avatar').style = "background-color:#"+randomColor();
-document.getElementById('fullname').innerHTML = fn;
-document.getElementById('username').innerHTML = un;
-document.getElementById('follower_number').innerHTML = Math.floor(Math.random() * 1000);
-document.getElementById('tweet_number').innerHTML = Math.floor(Math.random() * 1000);
-document.getElementById('following_number').innerHTML = Math.floor(Math.random() * 1000);
-document.getElementById('desc_ription').innerHTML = description();
-
+	document.getElementById('avatar').style = "background-color:#"+randomColor();
+	document.getElementById('fullname').innerHTML = fn;
+	document.getElementById('username').innerHTML = un;
+	document.getElementById('follower_number').innerHTML = Math.floor(Math.random() * 1000);
+	document.getElementById('tweet_number').innerHTML = Math.floor(Math.random() * 1000);
+	document.getElementById('following_number').innerHTML = Math.floor(Math.random() * 1000);
+	document.getElementById('desc_ription').innerHTML = description();
 };
