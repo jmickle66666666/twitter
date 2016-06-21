69a0e792e4f1f45addcfd926a1bb4eb1273f7cc6
Brandon Mulcahy <brandon@jangler.info>
2016-06-21 11:25:48 -0400

    Use double quotes in quotes array when quoting quotes that contain single quotes
    This reduces bloat and makes the quote strings in the source code easier
    to read.

--- description.js
+++ description.js
@@ -1,9 +1,9 @@
 var descripts = ['Sassy','Visionary','Clever','Cars','Computers','Coffee','Cappucinos','Tech','Games','Videogames','Culture','Savant','Guru','Idiot','Telepath','Constipated','Hunter','Force of Nature','Teen','Cyclist','Vegan','Naturist','Mediator','Journalist','Developer','Polymath','Passionate','Creative','Strategist','Leader','Enabler','Deviant','Producer','Futurist','Nerd','Geek','Designer'];
 var spacers = ['. ',', ',' . ',' | ',' + ',' & ',' : ',', ',', ','. '];
 var quotes = ['i would have extended that scene at the beginning of one of those game of thrones episodes where the knight is trying to light the longboat with a flaming arrow but keeps missing',
-			  'am I the only one that attributed meaning to the fact that invuln didn\'t change the sky color in vanilla doom engine games? :P',
-			  'rand paul literally said he wasn\'t brown enough to get held up at tsa',
-			  'am i the only person who finds it incredibly irritating when news websites make the left and right arrow keys take you to other articles? i don\'t always use the scrollwheel, and it\'s easy to accidentally press the wrong key and lose the page when navigating via arrow up/down',
+			  "am I the only one that attributed meaning to the fact that invuln didn't change the sky color in vanilla doom engine games? :P",
+			  "rand paul literally said he wasn't brown enough to get held up at tsa',
+			  "am i the only person who finds it incredibly irritating when news websites make the left and right arrow keys take you to other articles? i don't always use the scrollwheel, and it's easy to accidentally press the wrong key and lose the page when navigating via arrow up/down",
 			  'imperative form forces the message to be verb-initial which encourages you to actually descibe what was done imho',
 			  'i dontr think theres been a day in the last month where someone either aksed me what im applying for or told me what theyre applying for',
 			  'every time I go away for any amount of time I miss something amazing',
